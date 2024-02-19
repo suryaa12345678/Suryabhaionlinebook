86cb7db0b6fff9a79a8e815b532c81279d0b6193
Harshit <sharmaharshitraj@gmail.com>
2024-02-19 22:57:40 +0530

    Optimized

--- src/components/Offers.js
+++ src/components/Offers.js
@@ -6,7 +6,6 @@ export default function Offer() {
 
     return (
         <div className='offers'>
-            <img className="singleGirl" src="/singleGirl.webp" alt="Three Girls In red dress" />
 
             <div className="offersTransparent">
                 <div className='offersLeft'>
